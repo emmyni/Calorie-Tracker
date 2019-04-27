5ccfdbc0be26488495f7efae0500218f8388bd61
emmyni <emmyni220@gmail.com>
2019-04-27 14:12:12 -0400

    Setting up to push to Heroku

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/calories', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || 'localhost:27017/calories', {useNewUrlParser: true});
 var Schema = mongoose.Schema;
 
 /* GET home page. */
